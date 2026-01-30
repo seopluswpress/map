327d2421950012b9c7ddebc65fa4fd651f7b59a4
seopluswpress <seopluswpress@gmail.com>
2026-01-30 11:58:50 +0530

    Fix typo in playwright install command

--- render-build.sh
+++ render-build.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
-# exit on error
 set -o errexit
-
 pip install -r requirements.txt
-python -m playwright install --with-deps chromiu
+python -m playwright install --with-deps chromium
+chmod +x render-build.sh
